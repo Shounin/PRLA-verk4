e91f725512a54d75e7381817606d3d78508f5821
Shounin <gstyrmir@gmail.com>
2015-05-08 20:56:44 +0000

    added stuff

--- main.py
+++ main.py
@@ -16,12 +16,37 @@ def sort_downloads(src, dest, aliases):
                 files.append([os.path.join(dirName, x) for x in fileList])
                 continue
             files.append([os.path.join(dirName, x) for x in fileList if a.lower() in x.lower()])
-    print(set([x for sublist in files for x in sublist]))
+    sorted_list = sort_by_season([x for sublist in files for x in sublist])
+    try:
+        b = os.path.abspath(os.path.join(dest, aliases[0]))
+        print(b)
+        os.mkdir(b)
+    except:
+        pass
+    for key in sorted_list:
+        try:
+            a = os.path.abspath(os.path.join(os.path.join(dest, aliases[0]), key))
+            print(a)
+            os.mkdir(a)
+        except:
+            pass
+        for x in sorted_list[key]:
+            shutil.copy2(x, os.path.abspath(os.path.join(os.path.join(os.path.join(dest, aliases[0]), key), x.split('\\')[-1])))
+    return
 
 
 def sort_by_season(episode_list):
-    #TODO: implement
-    return
+    seasonPattern = re.compile(r's\s?(\d?\d)\s?e\d?\d', re.I)
+    seasons = {}
+    for x in episode_list:
+        if seasonPattern.findall(x):
+            s = 'Season ' + ''.join(seasonPattern.findall(x))
+            seasons[s] = seasons.get(s, []) + [x]
+        else:
+            s = 'Season ' + ''.join(seasonPattern.findall(x))
+            seasons['Misc'] = seasons.get(s, []) + [x]
+    print(seasons)
+    return seasons
 
 
 src = input("Enter source folder\n")
