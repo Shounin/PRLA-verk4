b9cd84fb05c6d8177fce7286bd2b132d92c9056e
Shounin <gstyrmir@gmail.com>
2015-05-09 14:42:57 +0000

    added more stuff

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-#
-#
-#
-#  TODO: README
-#
-#
-#
 import os, shutil, sys, re
 from glob import glob
+from pprint import pprint
 
 def sort_downloads(src, dest, aliases):
     files = []
@@ -17,35 +11,51 @@ def sort_downloads(src, dest, aliases):
                 continue
             files.append([os.path.join(dirName, x) for x in fileList if a.lower() in x.lower()])
     sorted_list = sort_by_season([x for sublist in files for x in sublist])
-    try:
-        b = os.path.abspath(os.path.join(dest, aliases[0]))
-        print(b)
-        os.mkdir(b)
-    except:
-        pass
+
+    # Copying and deleting
     for key in sorted_list:
-        try:
-            a = os.path.abspath(os.path.join(os.path.join(dest, aliases[0]), key))
-            print(a)
-            os.mkdir(a)
-        except:
-            pass
+        if key is not 'Misc':
+            try:
+                a = os.path.abspath(os.path.join(os.path.join(dest, aliases[0]), key))
+                #print(a)
+                os.makedirs(a)
+                #pprint(sorted_list[key])
+            except:
+                pass
+
         for x in sorted_list[key]:
-            shutil.copy2(x, os.path.abspath(os.path.join(os.path.join(os.path.join(dest, aliases[0]), key), x.split('\\')[-1])))
+            if key == 'Misc':
+                try:
+                    shutil.copy2(x, os.path.abspath(os.path.join(dest, aliases[0])))
+                    os.remove(os.path.abspath(x))
+                except FileNotFoundError:
+                    pass
+            #print(x)
+            try:
+                shutil.copy2(x, os.path.abspath(os.path.join(os.path.join(dest, aliases[0]), key)))
+                os.remove(os.path.abspath(x))
+            except FileNotFoundError:
+                pass
+            #pprint('tokst að eyða: ---> %s' % os.path.abspath(x))
+    #######
+
     return
 
 
 def sort_by_season(episode_list):
-    seasonPattern = re.compile(r's\s?(\d?\d)\s?e\d?\d', re.I)
+    seasonPattern1 = re.compile(r's\s?(\d?\d)\s?e\d?\d', re.I)
+    seasonPattern2 = re.compile(r'(\d?\d)x\d?\d', re.I)
     seasons = {}
     for x in episode_list:
-        if seasonPattern.findall(x):
-            s = 'Season ' + ''.join(seasonPattern.findall(x))
+        if seasonPattern1.findall(x):
+            s = 'Season ' + str(int(seasonPattern1.findall(x)[0]))
+            seasons[s] = seasons.get(s, []) + [x]
+        elif seasonPattern2.findall(x):
+            s = 'Season ' + str(int(seasonPattern2.findall(x)[0]))
             seasons[s] = seasons.get(s, []) + [x]
         else:
-            s = 'Season ' + ''.join(seasonPattern.findall(x))
-            seasons['Misc'] = seasons.get(s, []) + [x]
-    print(seasons)
+            seasons['Misc'] = seasons.get('Misc', []) + [x]
+    #print(seasons)
     return seasons
 
 
