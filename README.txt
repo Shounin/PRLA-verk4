91047e466f5c540862a2d1f3ff181d0a368335a8
Shounin <gstyrmir@gmail.com>
2015-05-10 20:18:24 +0000

    added much more stuff

---
+++ README.txt
@@ -0,0 +1,12 @@
+Usage directions:
+The script is meant to find all episodes of a particular television show inside a folder and move them to a separate folder, sorted by season.
+The user begins by entering the path to a source folder (e.g. "downloads") and then a destination folder (e.g. "shows").  The user then enters the name of a show.
+The first entry written in the list of possible names of the show will be the name given to the show's folder in the destination directory.  Then follow a number of possible aliases for the show, separated by a comma(',').
+For the show "Top Gear", the entry could look something like this: "Top Gear, top.gear, top_gear".  The search terms are not case sensitive.
+The script should then be able to find all episodes of the given show, and store them according to season.  If the script cannot determine a season for some files, those files will be moved to the root folder of the show, and the user could then sort them manually.
+One last feature is that the script also runs cleanup, removing all files of types .txt, .torrent, .dat, .rar and more.  Please remember to unrar files after downloading, before running the script
+
+Known bugs and limitations:
+On rare occasions, the script will create an empty file with no file extension instead of a folder in the destination folder.  The user just has to delete the file manually and try again, and the script should work.
+On even rarer occasions, the script will create such a file, and therefore not move the files, but remove them instead.
+The script also does not work for episodes which do not either contain the name of the show in the file name or in the name of their parent directory (such as "That '70s Show" in the example data given).
--- main.py
+++ main.py
@@ -1,47 +1,44 @@
-import os, shutil, sys, re
-from glob import glob
-from pprint import pprint
-
+import os, shutil, re
+ 
 def sort_downloads(src, dest, aliases):
     files = []
+    #Create a list of all files in the source directory
     for dirName, subdirList, fileList in os.walk(src):
         for a in aliases:
+
+            #copies all files in a directory if an alias is in the directory name
             if a.lower() in dirName.lower():
                 files.append([os.path.join(dirName, x) for x in fileList])
                 continue
             files.append([os.path.join(dirName, x) for x in fileList if a.lower() in x.lower()])
     sorted_list = sort_by_season([x for sublist in files for x in sublist])
-
+ 
     # Copying and deleting
     for key in sorted_list:
         if key is not 'Misc':
             try:
                 a = os.path.abspath(os.path.join(os.path.join(dest, aliases[0]), key))
-                #print(a)
                 os.makedirs(a)
-                #pprint(sorted_list[key])
             except:
                 pass
-
+ 
         for x in sorted_list[key]:
             if key == 'Misc':
                 try:
-                    shutil.copy2(x, os.path.abspath(os.path.join(dest, aliases[0])))
-                    os.remove(os.path.abspath(x))
-                except FileNotFoundError:
+                    shutil.move(x, os.path.join(dest, aliases[0]))
+                except:
                     pass
-            #print(x)
-            try:
-                shutil.copy2(x, os.path.abspath(os.path.join(os.path.join(dest, aliases[0]), key)))
-                os.remove(os.path.abspath(x))
-            except FileNotFoundError:
-                pass
-            #pprint('tokst að eyða: ---> %s' % os.path.abspath(x))
+            else:
+                try:
+                    shutil.move(x, os.path.join(os.path.join(dest, aliases[0]), key))
+                except:
+                    pass
     #######
-
+    cleanup(src)
+ 
     return
-
-
+ 
+#Returns a dictionary of episodes sorted by seasons.  If no season can be determined, the file is sorted with 'Misc'
 def sort_by_season(episode_list):
     seasonPattern1 = re.compile(r's\s?(\d?\d)\s?e\d?\d', re.I)
     seasonPattern2 = re.compile(r'(\d?\d)x\d?\d', re.I)
@@ -55,12 +52,26 @@ def sort_by_season(episode_list):
             seasons[s] = seasons.get(s, []) + [x]
         else:
             seasons['Misc'] = seasons.get('Misc', []) + [x]
-    #print(seasons)
     return seasons
-
-
+ 
+#Deletes unnecessary files and empty folders
+def cleanup(src):
+    ending = re.compile(r'\.txt|\.torrent|\.dat|\.rar|\.nfo|\.jpg|\.r\d\d|\.sfv$')
+   
+    for dirName, subdirList, fileList in os.walk(src, topdown=False):
+        for name in fileList:
+            if ending.findall(name):
+                os.remove(os.path.join(dirName, name))
+       
+        for name in subdirList:
+            if not os.listdir(os.path.join(dirName, name)):
+                os.rmdir(os.path.join(dirName, name))
+               
+ 
+ 
+##########################################################################################
 src = input("Enter source folder\n")
 dest = input("Enter destination folder\n")
 aliases = input("Enter name of show and aliases(optional), separated by ','\n").split(',')
-
+ 
 sort_downloads(src, dest, [i.strip() for i in aliases])
